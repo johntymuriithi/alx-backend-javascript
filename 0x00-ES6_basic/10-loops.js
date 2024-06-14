53e052cb87f780ef5a3413396812a3ee6cb448a9
johntymuriithi <muriithijohn634@gmail.com>
2024-06-14 22:20:05 +0300

    alx modern js

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [];
-  for (let i = 0; i < array.length; i += 1) {
-    newArray.push(appendString + array[i]);
+
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
 
   return newArray;
