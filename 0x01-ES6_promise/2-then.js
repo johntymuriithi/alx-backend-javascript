55dfabb7b9c9e850b6fecdbbab047c70ddad2647
johntymuriithi <muriithijohn634@gmail.com>
2024-06-17 08:38:39 +0300

    Don't promise if can't keep the promise

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,3 +1,7 @@
+// 2-then.js
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' })).catch(() => new Error()).finally(() => console.log('Got a response from the API'));
+  return promise
+    .then(() => ({ status: 200, body: 'Success' }))
+    .catch(() => new Error())
+    .finally(() => console.log('Got a response from the API'));
 }
