8f17c0e6418103ce6ce6749950520ccc2a13ad24
johntymuriithi <muriithijohn634@gmail.com>
2024-06-17 09:42:16 +0300

    Don't promise if can't keep the promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,3 +1,9 @@
-import { uploadPhoto, createUser } from "./utils";
+import { uploadPhoto, createUser } from './utils';
 
-e
+export default function handleProfileSignup() {
+  Promise.all([uploadPhoto(), createUser()]).then((data) => {
+    const [profile, user] = data;
+    console.log(`${profile.body} ${user.firstName} ${user.lastName}`);
+  })
+    .catch(() => console.log('Signup system offline'));
+}
