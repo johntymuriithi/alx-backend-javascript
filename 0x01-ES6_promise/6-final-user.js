b88ba649f94141cc9072267fe99c5fb78fa61238
johntymuriithi <muriithijohn634@gmail.com>
2024-06-17 11:07:16 +0300

    Don't promise if can't keep the promise

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,14 +1,15 @@
-import uploadPhoto from './5-photo-reject';
+/* eslint-disable no-unused-expressions */
 import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-// Define handleProfileSignup function
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // Call both functions and wait for them to settle using Promise.allSettled
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return { status: 'fulfilled', value: result.value };
-      }
-      return { status: 'rejected', value: result.reason };
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((data) => {
+    return data.map((item) => ({
+      status: item.status,
+      value: item.status === 'fulfilled' ? item.value : String(item.reason),
     }));
+  });
 }
